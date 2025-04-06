e4839f1f958c5918624ef21a8d5ddc711afb70bf
Nastya3456-fed <Nasty3256@yandex.ru>
2025-04-06 10:10:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,58 @@
 // Copyright 2021 NNTU-CS
+#include <iostream>
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
+  int c = 0;
+  int i = 0;
+  for (i; i < len; i++) {
+    for (int j = i + 1; j < len; j++) {
+      if (arr[i] + arr[j] == value) {
+        c=c+1;
+      }
+    }
+  }
+  return c;
 }
 int countPairs2(int *arr, int len, int value) {
-  return 0;
+  int c = 0;
+  int l = 0;
+  int r = len - 1;
+  while (l < r) {
+    int sloj = arr[l] + arr[r];
+    if (sloj == value) {
+      c++;
+      l++;
+      r--;
+    }
+    else if (sloj < value) {
+      l++;
+    }
+    else {
+      r--;
+    }
+ }
+return c;
 }
 int countPairs3(int *arr, int len, int value) {
-  return 0;
+  int c = 0;
+  int i = 0;
+  for (i; i < len; i++) {
+    int iscom = value - arr[i];
+    int l = i + 1;
+    int r = len - 1;
+    while (l <= r) {
+      int sr = l + (r - l) / 2;
+      if (arr[sr] == iscom) {
+        c++;
+        break;
+      }
+      else if (arr[sr] < iscom) {
+        l = sr + 1;
+      }
+      else {
+        r = sr - 1;
+      }
+    }
+    }
+  return c;
 }
