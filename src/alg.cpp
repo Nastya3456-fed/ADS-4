ded737ac0cae7f7baa412f7af46bc9d7a87cfc89
Nastya3456-fed <Nasty3256@yandex.ru>
2025-04-06 18:32:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -19,24 +19,23 @@ int countPairs1(int *arr, int len, int value) {
 int countPairs2(int *arr, int len, int value) {
   std::sort(arr, arr + len);
   int c = 0;
-  for (int i = 0; i < len; i++) {
-    int iscom = value - arr[i];
-    int l = i + 1;
-    int r = len - 1;
-    while (l <= r) {
-      int sr = l + (r - l) / 2;
-      if (arr[sr] == iscom) {
-        c++;
-        break;
-      } else if (arr[sr] < iscom) {
-        l = sr + 1;
-      } else {
-        r = sr - 1;
-      }
+  int l = 0;
+  int r = len - 1;
+  while (l < r) {
+    int sloj = arr[l] + arr[r];
+    if (sloj == value) {
+      c++;
+      l++;
+      r--;
+      while (l < r && arr[l] == arr[l - 1]) l++;
+      while (l < r && arr[r] == arr[r + 1]) r--;
+    } else if (sloj < value) {
+      l++;
+    } else {
+      r--;
     }
-    while (i + 1 < len && arr[i] == arr[i + 1]) i++;
-    }
-  return c;
+  }
+return c;
 }
 int countPairs3(int *arr, int len, int value) {
   std::sort(arr, arr + len);
